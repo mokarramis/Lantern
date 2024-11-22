d9f6f0f93de6830681704327987f016a73e6b82a
mokarramis <sahar.mokarrami94@gmail.com>
2024-11-22 22:45:51 +0330

    add asset crud

---
+++ src/app/Http/Controllers/AssetController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Asset\AssetRequest;
+use App\Models\Asset;
+use App\Repositories\AssetRepository;
+use App\Respondors\AssetRespondor;
+
+class AssetController extends Controller
+{
+    public function __construct(public AssetRepository $assetRepository, public AssetRespondor $assetRespondor)
+    {
+        
+    }
+
+
+    public function store(AssetRequest $request)
+    {
+        $data = $request->validated();
+        $asset = $this->assetRepository->create($data);
+
+        return $this->assetRespondor->respondResource($asset, 200);
+    }
+
+    public function update(AssetRequest $request, Asset $asset)
+    {
+        $data = $request->validated();
+        $asset = $asset->update($data);
+
+        return $this->assetRespondor->respondResource($asset, 200);
+    }
+
+    public function destroy(Asset $asset)
+    {
+        $asset = $asset->delete();
+
+        return $this->assetRespondor->respondResource($asset, 200);
+    }
+}

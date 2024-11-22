3a3ab425c6470fd0f5397f5409debcedb89d72cf
mokarramis <sahar.mokarrami94@gmail.com>
2024-11-22 13:32:11 +0330

    create models and migrations related to assets

--- src/database/migrations/2024_11_22_091815_create_cashes_table.php
+++ src/database/migrations/2024_11_22_091815_create_cashes_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('oauth_personal_access_clients', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('client_id');
+        Schema::create('cashes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('resource');
+            $table->bigInteger('amount')->default(0);
+            $table->mediumText('other')->nullable();
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('oauth_personal_access_clients');
+        Schema::dropIfExists('cashes');
     }
 };
